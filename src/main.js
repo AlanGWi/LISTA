0d5f0fbb9e15e88bca5d3fa03c6e1ccd60b9aea4
AlanGWi <115105025+AlanGWi@users.noreply.github.com>
2025-08-29 20:57:17 -0400

    raiz

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // ✅ Importar Bootstrap y estilos
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap/dist/js/bootstrap.bundle.min"
 
 createApp(App).mount('#app')
